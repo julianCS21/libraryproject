605e0c2f10cc40e1b8b5852af915371844e97b76
juan-bazurto-arkdia <juan.bazurto@arkdia.co>
2024-05-05 19:49:10 -0500

    Unit test of Author update

--- src/test/java/com/ECI/book/library/AuthorTest.java
+++ src/test/java/com/ECI/book/library/AuthorTest.java
@@ -1,60 +1,66 @@
 package com.ECI.book.library;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Date;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.ECI.book.library.domain.model.Author;
 import org.junit.jupiter.api.Test;
+import java.util.Date;
 
 public class AuthorTest {
 
     @Test
     public void testNoArgsConstructor() {
         Author author = new Author();
-        assertEquals(null, author.getId());
-        assertEquals(null, author.getBiography());
+        assertNull(author.getId());
+        assertNull(author.getBiography());
         assertEquals(0, author.getAge());
-        assertEquals(null, author.getBirthday());
+        assertNull(author.getBirthday());
     }
 
     @Test
     public void testAllArgsConstructor() {
-        Date date = new Date();
-        Author author = new Author("1", "biography", 30, date);
+        Date birthday = new Date();
+        Author author = new Author("1", "Some biography", 30, birthday);
         assertEquals("1", author.getId());
-        assertEquals("biography", author.getBiography());
+        assertEquals("Some biography", author.getBiography());
         assertEquals(30, author.getAge());
-        assertEquals(date, author.getBirthday());
-    }
-
-    @Test
-    public void testBuilder() {
-        Date date = new Date();
-        Author author = Author.builder()
-                .id("1")
-                .biography("biography")
-                .age(30)
-                .birthday(date)
-                .build();
-
-        assertEquals("1", author.getId());
-        assertEquals("biography", author.getBiography());
-        assertEquals(30, author.getAge());
-        assertEquals(date, author.getBirthday());
+        assertEquals(birthday, author.getBirthday());
     }
 
     @Test
     public void testSettersAndGetters() {
         Author author = new Author();
         author.setId("2");
-        author.setBiography("new bio");
+        author.setBiography("Another biography");
         author.setAge(25);
-        Date newDate = new Date();
-        author.setBirthday(newDate);
+        Date newBirthday = new Date();
+        author.setBirthday(newBirthday);
 
         assertEquals("2", author.getId());
-        assertEquals("new bio", author.getBiography());
+        assertEquals("Another biography", author.getBiography());
         assertEquals(25, author.getAge());
-        assertEquals(newDate, author.getBirthday());
+        assertEquals(newBirthday, author.getBirthday());
+    }
+
+    @Test
+    public void testEquals() {
+        Author author1 = new Author("1", "Bio", 40, new Date());
+        Author author2 = new Author("1", "Bio", 40, new Date());
+        assertTrue(author1.equals(author2) && author2.equals(author1));
+        assertEquals(author1.hashCode(), author2.hashCode());
+    }
+
+    @Test
+    public void testNotEquals() {
+        Author author1 = new Author("1", "Bio", 40, new Date());
+        Author author2 = new Author("2", "Bio", 40, new Date());
+        assertFalse(author1.equals(author2));
+    }
+
+    @Test
+    public void testToString() {
+        Author author = new Author("1", "Bio", 40, new Date());
+        String expected = "Author(id=1, biography=Bio, age=40, birthday=" + author.getBirthday() + ")";
+        assertEquals(expected, author.toString());
     }
 }
